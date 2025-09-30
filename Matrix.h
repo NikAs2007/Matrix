1f2e4353ea08c897bd7f464a15220660c3990016
SashaWindows <nikonorovsasha1@gmail.com>
2025-09-30 16:13:12 +0300

    Первая версия

---
+++ Matrix.h
@@ -0,0 +1,58 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Matrix {
+	vector<vector<int>> matrix;
+
+	int det(vector<vector<int>>& mat);
+	class HelperString {
+		vector<int>* string;
+	public:
+		HelperString(vector<int>* str) : string(str) {}
+		int& operator[](int j) {
+			return (*string)[j];
+		}
+	};
+public:
+
+	Matrix(){}
+
+	~Matrix(){}
+
+	Matrix(const vector<vector<int>>& mat);
+
+	Matrix(vector<vector<int>>&& mat) noexcept;
+
+	Matrix(const Matrix& mat);
+
+	Matrix(Matrix&& mat) noexcept;
+
+	void operator =(const Matrix& right);
+
+	void operator =(Matrix&& right) noexcept;
+
+	Matrix input_matrix();
+
+	void print_matrix();
+
+	int det();
+
+	pair<int, int> size();
+
+	Matrix operator +(Matrix& right);
+
+	HelperString& operator [](int i) {
+		HelperString h = HelperString(&matrix[i]);
+		return h;
+	}
+};
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+﻿#include "Matrix.h"
+
+int main()
+{
+
+    return 0;
+}
